752f967ae91ed9123ecd051eaffa50fd0e14909f
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2023-11-20 11:40:30 +0100

    Added definition canMoveTo and place and all declaration of implementation WorldMap in RectuangularMap methods

---
+++ animals/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -0,0 +1,35 @@
+package agh.ics.oop.model;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RectangularMap implements WorldMap {
+    private Map<Vector2d, Animal> animals = new HashMap<>();
+    private final Vector2d lowerLeft;
+    private final Vector2d upperRight;
+    RectangularMap(int width, int height){
+        lowerLeft = new Vector2d(0,0);
+        upperRight = new Vector2d(width - 1,height - 1);
+    }
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        return upperRight.follows(position) && lowerLeft.precedes(position) && !animals.containsKey(position);
+    }
+    @Override
+    public boolean place(Animal animal) {
+        if(canMoveTo(animal.getPosition()) && animal.getOrientation() == MapDirection.NORTH
+                && !animals.containsKey(animal.getPosition())){
+            animals.put(animal.getPosition(),animal);
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public void move(Animal animal, MoveDirection direction){}
+
+    @Override
+    public boolean isOccupied(Vector2d position) {}
+
+    @Override
+    public Animal objectAt(Vector2d position) {}
+}

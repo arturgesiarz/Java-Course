764bac41b5c243b625264afa673d1320e4f3d29c
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2023-12-03 22:54:43 +0100

    Added to OptionsParser condition against uncorrect arg

--- animals/src/main/java/agh/ics/oop/OptionsParser.java
+++ animals/src/main/java/agh/ics/oop/OptionsParser.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OptionsParser {
-    public static List<MoveDirection> giveDirections(String[] args) {
+    public static List<MoveDirection> giveDirections(String[] args){
         List<MoveDirection> moves = new ArrayList<>();
 
         for(String arg : args){
@@ -14,6 +14,7 @@ public class OptionsParser {
                 case "b" -> moves.add(MoveDirection.BACKWARD);
                 case "l" -> moves.add(MoveDirection.LEFT);
                 case "r" -> moves.add(MoveDirection.RIGHT);
+                default -> throw new IllegalArgumentException(arg + " is not legal move specification");
             }
         }
         return moves;

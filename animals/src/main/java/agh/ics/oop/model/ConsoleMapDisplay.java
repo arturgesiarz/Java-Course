826381d7f1a20731528db58b4bc8be5f3d595517
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2023-12-04 10:47:41 +0100

    Created class ConsoleMapDisplay

---
+++ animals/src/main/java/agh/ics/oop/model/ConsoleMapDisplay.java
@@ -0,0 +1,8 @@
+package agh.ics.oop.model;
+
+public class ConsoleMapDisplay implements MapChangeListener{
+    @Override
+    public void mapChanged(WorldMap worldMap, String message) {
+
+    }
+}

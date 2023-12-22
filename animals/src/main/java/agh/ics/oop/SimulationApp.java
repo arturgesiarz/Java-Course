95c49f6a12f7e10ba0b78a3155ffc8930464fe37
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2023-12-22 19:51:07 +0100

    Added SimulationPresenter class and added configureStage method in SimulationApp

--- animals/src/main/java/agh/ics/oop/SimulationApp.java
+++ animals/src/main/java/agh/ics/oop/SimulationApp.java
@@ -1,9 +1,26 @@
 package agh.ics.oop;
+import agh.ics.oop.presenter.SimulationPresenter;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class SimulationApp extends Application {
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml")); //tu nastepuje pobieranie informacji z fxml
+        BorderPane viewRoot = loader.load(); //tworze tutaj swoj widok
+        SimulationPresenter presenter = loader.getController();
+        configureStage(primaryStage, viewRoot); //konfiguruje moj glowny widok w tym miejscu
         primaryStage.show();
+    }
+    private void configureStage(Stage primaryStage, BorderPane viewRoot) {
+        Scene scene = new Scene(viewRoot);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Simulation app");
+        primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
+        primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
     }
 }

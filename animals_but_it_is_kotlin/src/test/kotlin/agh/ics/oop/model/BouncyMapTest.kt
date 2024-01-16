82e1ba5caaa8bdd2a8cd0c673f3237c303d60fa1
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-01-16 19:49:28 +0100

    Test coming

---
+++ animals_but_it_is_kotlin/src/test/kotlin/agh/ics/oop/model/BouncyMapTest.kt
@@ -0,0 +1,18 @@
+package agh.ics.oop.model
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class BouncyMapTest {
+
+    fun place() {
+        TODO()
+    }
+
+    fun objectAt() {
+        TODO()
+    }
+
+    fun canMoveTo() {
+        TODO()
+    }
+}
